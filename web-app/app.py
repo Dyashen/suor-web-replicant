374438d172c998bd7de817c30d6fa2d6d8ea6d06
Dylan Cluyse <dylan.cluyse@student.hogent.be>
2023-05-09 21:23:52 +0200

    update

--- web-app/app.py
+++ web-app/app.py
@@ -5,12 +5,11 @@ app = Flask(__name__)
 
 def connection():
     
-    DB_NAME='depdatabase'
     DB_NAME=os.getenv('DB_NAME')
-    DB_USER='postgres'
-    DB_HOST='vichogent.be'
-    DB_PORT='40033'
-    DB_PASSWORD='DaddySzymon123'
+    DB_USER=os.getenv('DB_USER')
+    DB_HOST=os.getenv('DB_HOST')
+    DB_PORT=os.getenv('DB_PORT')
+    DB_PASSWORD=os.getenv('DB_PASSWORD')
     conn = psycopg2.connect(dbname=DB_NAME,user=DB_USER,host=DB_HOST,port=DB_PORT,password=DB_PASSWORD)
     return conn
 
